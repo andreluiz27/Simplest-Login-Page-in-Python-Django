526ec3708459601705012897436fec8f3d81cb3b
andreluiz27 <52846311+andreluiz27@users.noreply.github.com>
2019-08-19 23:51:20 -0300

    Add files via upload

--- login_web/users/models.py
+++ login_web/users/models.py
@@ -3,8 +3,9 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    cpf = models.CharField(max_length=11, blank=True,unique=True)
-    cellNumber=models.CharField(max_length=11,blank=True)
+    cpf = models.CharField(max_length=11,blank=False,unique=True,help_text="Não colocar traços")
+    cellNumber=models.CharField(max_length=11,blank=False,unique=True,help_text="Exemplo: 16998801948")
+    #email = models.EmailField(max_length=70, blank=True)
 
     def __str__(self):
         return self.email
